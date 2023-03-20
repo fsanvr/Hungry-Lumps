ac66a4886e278a10be541174677d447d4774b1c0
fs.anvr <fs.anvr@gmail.com>
2023-03-20 14:01:25 +1000

    implemented player move system

---
+++ Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class MoveSystem : MonoBehaviour
+{
+    private GameField _gameField;
+    private Player _player;
+
+    private void Start()
+    {
+        _gameField = GameObject.Find("GameField").GetComponent<GameField>();
+        _player = this.GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        ProcessMove();
+    }
+
+    private void ProcessMove()
+    {
+        if (IsMouseInput())
+        {
+            var screenPosition = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+            var clicked = GetClickedObject(screenPosition);
+            if (clicked && IsCell(clicked))
+            {
+                var cell = clicked.GetComponent<Cell>();
+                if (_gameField.IsNeighbour(_player.transform.position, cell))
+                {
+                    MovePlayerTo(cell.position);
+                }
+            }
+        }
+    }
+
+    private bool IsMouseInput()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+
+    private GameObject GetClickedObject(Vector2 clickPosition)
+    {
+        var direction = Vector2.zero;
+        var hit = Physics2D.Raycast(clickPosition, direction);
+        if (hit)
+        {
+            return hit.collider.gameObject;
+        }
+
+        return null;
+    }
+
+    private bool IsCell(GameObject go)
+    {
+        return go.GetComponent<Cell>() != null;
+    }
+
+    private void MovePlayerTo(Vector2 position)
+    {
+        _player.MoveTo(position);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerData data;
+
+    public void MoveTo(Vector2 position)
+    {
+        this.transform.position = position;
+        data.position = position;
+    }
+}
