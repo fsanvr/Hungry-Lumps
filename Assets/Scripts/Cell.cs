665c4082f4ab36ed5222edb4ea245d57aae0328a
fs.anvr <fs.anvr@gmail.com>
2023-03-20 14:01:12 +1000

    implemented game field initialization

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Vector2 position;
+}
---
+++ Assets/Scripts/GameField.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class GameField : MonoBehaviour
+{
+    public LevelInitData levelInitData;
+    private Cell[,] _cells;
+    
+    public bool IsNeighbour(Vector2 position, Cell cell)
+    {
+        const float epsilon = 0.01f;
+        return Math.Abs((position - cell.position).magnitude - 1.0f) < epsilon;
+    }
+    private void Start()
+    {
+        InitField();
+    }
+
+    private void InitField()
+    {
+        var fieldShape = levelInitData.gameFieldShape;
+        _cells = new Cell[fieldShape.x, fieldShape.y];
+        foreach (var x in Enumerable.Range(0, fieldShape.x))
+        {
+            foreach (var y in Enumerable.Range(0, fieldShape.y))
+            {
+                var position = new Vector2Int(x, y);
+                InitCell(levelInitData.cellPrefab, position);
+            }
+        }
+    }
+
+    private void InitCell(GameObject prefab, Vector2Int position)
+    {
+        var cell = Instantiate(prefab, new Vector3(position.x, position.y, 0),
+            Quaternion.identity);
+        var cellComponent = cell.GetComponent<Cell>();
+        
+        _cells[position.x, position.y] = cellComponent;
+        cellComponent.position = cell.transform.position;
+        cell.transform.parent = this.transform;
+    }
+}
