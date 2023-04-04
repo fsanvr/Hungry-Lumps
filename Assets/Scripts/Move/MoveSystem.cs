2f218af269c9792b1fae96429bdd0f4a249afdb7
fs.anvr <fs.anvr@gmail.com>
2023-04-04 10:27:55 +1000

    added move delay

--- Assets/Scripts/Move/MoveSystem.cs
+++ Assets/Scripts/Move/MoveSystem.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
 {
@@ -7,16 +6,33 @@ public class MoveSystem : MonoBehaviour
     private GameField _gameField;
     private Player _player;
 
+    [SerializeField] private float moveDelayInSeconds = 3.5f;
+    private float _currentDelay = 0.0f;
+    
     private void Start()
     {
         input.MouseClicked.AddListener(OnMouseClicked);
         _gameField = GameObject.Find("GameField").GetComponent<GameField>();
         _player = this.GetComponent<Player>();
     }
-
+    
     private void OnDestroy()
     {
         input.MouseClicked.RemoveListener(OnMouseClicked);
+    }
+    
+    private void Update()
+    {
+        ProcessDelay();
+    }
+
+    private void ProcessDelay()
+    {
+        if (_currentDelay >= 0.0f)
+        {
+            Debug.Log(_currentDelay);
+            _currentDelay -= Time.deltaTime;
+        }
     }
 
     private void OnMouseClicked(Collider2D clicked)
@@ -24,20 +40,27 @@ public class MoveSystem : MonoBehaviour
         if (IsCell(clicked.gameObject))
         {
             var cell = clicked.GetComponent<Cell>();
-            if (_gameField.IsNeighbour(_player.transform.position, cell))
+            if (_gameField.IsNeighbour(_player.transform.position, cell) &&
+                PlayerCanMove())
             {
                 MovePlayerTo(cell);
             }
         }
     }
-
+    
     private bool IsCell(GameObject go)
     {
         return go.GetComponent<Cell>() != null;
     }
+    
+    private bool PlayerCanMove()
+    {
+        return _currentDelay <= 0.0f;
+    }
 
     private void MovePlayerTo(Cell cell)
     {
+        _currentDelay = moveDelayInSeconds;
         _player.MoveTo(cell);
     }
 }
