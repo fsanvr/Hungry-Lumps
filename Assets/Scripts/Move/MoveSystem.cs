fc3fda732624770ab57f638fdd7e181b946a5e0d
fs.anvr <fs.anvr@gmail.com>
2023-04-12 22:36:21 +1000

    the movement system has been rewritten to initialize behavior

--- Assets/Scripts/Move/MoveSystem.cs
+++ Assets/Scripts/Move/MoveSystem.cs
@@ -1,21 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-public class MoveSystem : MonoBehaviour
+public class MoveSystem : InitializableBehaviour
 {
     [SerializeField] private InputSystem input;
-    private GameField _gameField;
-    private PlayerComponent _playerComponent;
+    [SerializeField] private SatietySystem satietySystem;
+    [SerializeField] private PlayerComponent playerComponent;
+    [SerializeField] private GridSystem gridSystem;
 
     [SerializeField] private float moveDelayInSeconds = 3.5f;
     private float _currentDelay = 0.0f;
-    
-    private void Start()
+
+    private float _moveCost;
+
+    [SerializeField] private BuffableSystemsDictionary dictionary;
+
+    protected override void MyInit(LevelData data)
     {
         input.MouseClicked.AddListener(OnMouseClicked);
-        _gameField = GameObject.Find("GameField").GetComponent<GameField>();
-        _playerComponent = this.GetComponent<PlayerComponent>();
+        _moveCost = 2.0f;
+        //_moveCost = data.Pet.MoveComponent.GetCostOfMove();
     }
-    
+
     private void OnDestroy()
     {
         input.MouseClicked.RemoveListener(OnMouseClicked);
@@ -39,7 +46,8 @@ public class MoveSystem : MonoBehaviour
         if (IsCell(clicked.gameObject))
         {
             var cell = clicked.GetComponent<Cell>();
-            if (_gameField.IsNeighbour(_playerComponent.transform.position, cell) &&
+            if (gridSystem.IsNeighbour(playerComponent.transform.position, cell) &&
+                cell.IsPassable() &&
                 PlayerCanMove())
             {
                 MovePlayerTo(cell);
@@ -47,19 +55,43 @@ public class MoveSystem : MonoBehaviour
         }
     }
     
-    private bool IsCell(GameObject go)
+    private static bool IsCell(GameObject go)
     {
         return go.GetComponent<Cell>() != null;
     }
     
     private bool PlayerCanMove()
     {
-        return _currentDelay <= 0.0f;
+        return _currentDelay <= 0.0f && satietySystem.CurrentSatiety >= _moveCost;
     }
 
     private void MovePlayerTo(Cell cell)
     {
+        if (cell.ContainsFood())
+        {
+            satietySystem.FillSatiety();
+            cell.ClearFood();
+        }
+        else
+        {
+            satietySystem.DecreaseSatiety(_moveCost);
+        }
+
+        if (cell.ContainsBonus())
+        {
+            ProcessBuff(cell.GetBonus());
+            cell.ClearBonus();
+        }
         _currentDelay = moveDelayInSeconds;
-        _playerComponent.MoveTo(cell);
+        playerComponent.transform.position = cell.position;
+    }
+
+    private void ProcessBuff(Buff buff)
+    {
+        var system = dictionary.GetValueOrDefault(buff.SystemName, null);
+        if (system)
+        {
+            ((Buffable)system).AddBuff(buff);
+        }
     }
 }
