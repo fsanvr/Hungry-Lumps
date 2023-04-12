b5f0f2324d84b172422e96ee0c8cb5d9d55c2468
fs.anvr <fs.anvr@gmail.com>
2023-04-12 22:25:15 +1000

    extended monobehaviour classes have been written

---
+++ Assets/Scripts/Behaviour/BlockableBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BlockableBehaviour : MonoBehaviour
+{
+    [SerializeField] private bool isBlocked;
+
+    public void Block()
+    {
+        isBlocked = true;
+    }
+
+    public void Unblock()
+    {
+        isBlocked = false;
+    }
+
+    public bool IsBlocked()
+    {
+        return isBlocked;
+    }
+}
---
+++ Assets/Scripts/Behaviour/InitializableBehaviour.cs
@@ -0,0 +1,37 @@
+﻿public abstract class InitializableBehaviour : BlockableBehaviour
+{
+    public void Init(LevelData data)
+    {
+        Unblock();
+        MyInit(data);
+    }
+    
+    private void Awake()
+    {
+        Block();
+    }
+
+    private void Start() { }
+
+    private void Update()
+    {
+        if (IsBlocked())
+        {
+            return;
+        }
+        MyUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsBlocked())
+        {
+            return;
+        }
+        MyFixedUpdate();
+    }
+
+    protected abstract void MyInit(LevelData data);
+    protected virtual void MyUpdate() { }
+    protected virtual void MyFixedUpdate() { }
+}
