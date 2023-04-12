d57f0b072945bb99f5cb1e7b1cbe405475252567
fs.anvr <fs.anvr@gmail.com>
2023-04-12 22:22:46 +1000

    a system of buffs has been written

---
+++ Assets/Scripts/Buff/Buff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine.Serialization;
+
+[Serializable]
+public class Buff
+{
+    public Func<float, float> Apply { get; private set; }
+    public string ApplyToName { get; private set; }
+    
+    public string SystemName { get => ApplyToName.Split(".")[0]; }
+    public string PropertyName { get => ApplyToName.Split(".")[1]; }
+
+    public Buff(Func<float, float> func, string name)
+    {
+        Apply = func;
+        ApplyToName = name;
+    }
+    
+    public float ApplyByName(float value, string name)
+    {
+        if (ApplyToName.Equals(name))
+        {
+            return Apply(value);
+        }
+
+        return value;
+    }
+}
