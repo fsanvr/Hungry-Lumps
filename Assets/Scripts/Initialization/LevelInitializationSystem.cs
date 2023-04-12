85567c1ed666a3d910f259294d973b0e58a33c2d
fs.anvr <fs.anvr@gmail.com>
2023-04-12 22:37:22 +1000

    the initialization system of gaming systems is simplified with initialized behavior

---
+++ Assets/Scripts/Initialization/LevelInitializationSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInitializationSystem : MonoBehaviour
+{
+    [SerializeField] private List<InitializableBehaviour> systemsToInit = new List<InitializableBehaviour>();
+    [SerializeField] private LevelData levelData;
+    private void Awake()
+    {
+        InitSystems();
+    }
+
+    private void InitSystems()
+    {
+        foreach (var system in systemsToInit)
+        {
+            system.Init(levelData);
+        }
+    }
+}
