57d0edb8d9f6c785eed6bc6a771874d9537faea1
fs.anvr <fs.anvr@gmail.com>
2023-04-27 20:47:02 +1000

    added new pets

--- Assets/Scripts/Pet/Pet.cs
+++ Assets/Scripts/Pet/Pet.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using UnityEditor.Animations;
+using UnityEngine;
 
 [CreateAssetMenu]
 public class Pet : ScriptableObject
 {
     [SerializeField] public string petName;
     [SerializeField] public Sprite sprite;
-    [SerializeField] public Animator animator;
+    [SerializeField] public AnimatorController animatorController;
     public SatietyComponent satietyComponent = new SatietyComponent();
     public MoveComponent moveComponent = new MoveComponent();
 }
