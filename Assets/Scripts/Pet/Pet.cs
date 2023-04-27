7a05b82b8edebfe53b47c164267ea29952306859
fs.anvr <fs.anvr@gmail.com>
2023-04-27 21:45:40 +1000

    improved UI

--- Assets/Scripts/Pet/Pet.cs
+++ Assets/Scripts/Pet/Pet.cs
@@ -1,12 +1,11 @@
-﻿using UnityEditor.Animations;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu]
 public class Pet : ScriptableObject
 {
     [SerializeField] public string petName;
     [SerializeField] public Sprite sprite;
-    [SerializeField] public AnimatorController animatorController;
+    [SerializeField] public RuntimeAnimatorController animatorController;
     public SatietyComponent satietyComponent = new SatietyComponent();
     public MoveComponent moveComponent = new MoveComponent();
 }
